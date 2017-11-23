9ff7c145828e08c889835ed773702457494f4e09
hx.weishuhua <15366181430@189.cn>
2017-11-23 17:39:08 +0800

    修改

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("This is my first GitHub project!");
+        System.out.print("This is my first GitHub project!!");
     }
 }
